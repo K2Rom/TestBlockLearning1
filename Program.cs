c667af388e2f565501f92799c3e93332332b8624
K2Rom <ivks@mail.ru>
2024-03-11 17:40:13 +0300

    добавлены методы

--- Program.cs
+++ Program.cs
@@ -14,24 +14,71 @@ int MaxLength = 3;
 // Метод формирования логического массива, где TRUE для строк, длиной меньше или равно допуску
 Boolean[] BooleanOfArray(string[] ArrayOfString, int MaxLength)
 {
+    int length = ArrayOfString.Length;
+    Boolean[] ArrayLength;
+    ArrayLength = new Boolean[length];
 
+    for (int i = 0; i < length; i++)
+    {
+        if (ArrayOfString[i].Length <= MaxLength)
+        {
+            ArrayLength[i] = true;
+        }
+        else
+        {
+            ArrayLength[i] = false;
+        }
+    }
+    return (ArrayLength);
 }
+
 
 // Метод подсчета количества строк, длиной меньше или равно допуску
 int CountBoolean(Boolean[] ArrayOfBoolean)
 {
 
+    int length = ArrayOfBoolean.Length;
+    int CountString = 0;
+    for (int i = 0; i < length; i++)
+    {
+        if (ArrayOfBoolean[i])
+            CountString++;
+
+    }
+    return (CountString);
 }
+
 
 // Метод формирования выходного массива, содержащего только строки, длиной меньше или равно допуску
 string[] CopyArrayString(string[] ArrayOfString, Boolean[] ArrayOfBoolean, string[] ArrayStringOut)
 {
-
+    int length = ArrayOfString.Length;
+    int j = 0;
+    for (int i = 0; i < length; i++)
+    {
+        if (ArrayOfBoolean[i])
+        {
+            ArrayStringOut[j] = ArrayOfString[i];
+            j++;
+        }
+    }
+    return (ArrayStringOut);
 }
+
 // Метод вывода массива строк на экран
 void WriteArray(string[] ArrayString)
 {
-
+    int length = ArrayString.Length;
+    Console.Write("[");
+    for (int i = 0; i < length; i++)
+    {
+        Console.Write($"“{ArrayString[i]}”");
+        if ((i + 1) != length)
+        {
+            Console.Write(", ");
+        }
+    }
+    Console.Write("]");
 }
 
 Boolean[] ArrayOfBoolean = BooleanOfArray(ArrayOfString, MaxLength);                // формирукм булевый массив
